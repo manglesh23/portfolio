163b5902a0ca41a51e8d5bb665dfb33078c724cd
manglesh23 <mangleshyadav2@gmail.com>
2024-11-26 10:33:08 +0530

    changes

--- website/src/App.jsx
+++ website/src/App.jsx
@@ -4,23 +4,21 @@ import Homepage from "./component/homepage";
 import About from "./Pages/About";
 import Projects from "./Pages/Projects";
 import Contact from "./Pages/Contact";
-import profileImage from "./assets/pic.jpeg";
-import ExpandableAvatar from "./component/ExpandableAvatar";
+
 
 function App() {
-  const [count, setCount] = useState(0);
+  
 
   return (
     <>
       <Routes>
-       
         <Route path="/" element={<Homepage />} />
         {/* <Route path="/home" element={<Homepage />} /> */}
-        <Route path="/home" element={<Navigate to="/" replace />} />
+        <Route path="/home" element={<Navigate to="/" replace />} />  
         {/* <Route path={["/", "/home"]} element={<Homepage />} /> */}
         <Route path="/about" element={<About />} />
         <Route path="/projects" element={<Projects />} />
-        <Route path="/contact" element={<Contact />} />
+        <Route path="/contact" element={<Contact />} /> 
       </Routes>
     </>
   );
