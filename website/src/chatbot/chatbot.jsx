7caf093e4054affbf9e8993318e4d27e8f342325
manglesh23 <mangleshyadav2@gmail.com>
2024-11-28 14:57:58 +0530

    intent training

--- website/src/chatbot/chatbot.jsx
+++ website/src/chatbot/chatbot.jsx
@@ -11,8 +11,10 @@ import {
   useColorModeValue,
 } from "@chakra-ui/react";
 import { FaPaperPlane, FaSun, FaMoon } from "react-icons/fa";
+import intents from "../helper/intent";
 
 const ChatBot = () => {
+  console.log("Intent:-", intents);
   const { toggleColorMode } = useColorMode();
   const bgColor = useColorModeValue("gray.100", "gray.900");
   const buttonColor = useColorModeValue("teal.900", "blue.100");
@@ -29,27 +31,6 @@ const ChatBot = () => {
   };
 
   const chatContainerRef = useRef(null);
-
-  const intents = {
-    "hello": "Hi there! 😊 How can I assist you?",
-    "hi": "Hi there! 😊 How can I assist you?",
-    "help": "Sure! Let me know what you need help with.",
-    "what is your name": "My name is GOLA AI",
-    "what is your anem": "My name is GOLA AI",
-    "how are you": "I'm good, what about you",
-    "how are you!": "I'm good, what about you",
-    "how are you doing": "I'm good, what about you",
-    "tell me about yourself":
-      "I'm in developing phase, You wanna know about my creater?",
-    yes: "He is from IIT Dhanbad, have worked for two start-ups, love to learn new tech like currently working on NLP to make me better",
-    "yes, sure":
-      "He is from IIT Dhanbad, have worked for two start-ups, love to learn new tech like currently working on NLP to make me better",
-    "go ahead":
-      "He is from IIT Dhanbad, have worked for two start-ups, love to learn new tech like currently working on NLP to make me better",
-    "download cv": "krta hu download.....",
-    "tell me about ashwani":"Kuch bolunga to vivad ho jayega",
-    "sach bolne se darr rhe ho?":"Rao Sahab GOLA kisi se nahi darta"
-  };
 
   async function callfunction(text) {
     try {
