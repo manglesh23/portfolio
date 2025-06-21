9633ce4ede21c5561cbe8b6ffa92d5d55fd62fe0
manglesh23 <mangleshyadav2@gmail.com>
2025-06-22 00:41:09 +0530

    AI Bot

--- backend/controller/gemini.js
+++ backend/controller/gemini.js
@@ -14,15 +14,17 @@ const gemini = async (req, res) => {
       "about your creater":
        "Manglesh Yadav, who built me, is a skilled and passionate backend developer with deep expertise in the MERN stack, especially proficient in Node.js, Express.js, and MongoDB. With a solid academic background from IIT Dhanbad and over 2.5 years of professional experience at innovative startups, he has built scalable, secure systems involving complex role hierarchies, real-time data handling, and third-party integrations such as AWS S3, Razorpay, and Aadhaar verification APIs. Manglesh is a self-driven problem solver who thrives on learning new technologies, recently exploring Generative AI and LangChain to integrate LLMs into real-world applications. His strong analytical mindset and eagerness to stay on the cutting edge make him a valuable asset in any tech-driven team.",
       "about you": "My name is GOLA, I'm a LLM",
-      "remendra ranjan":"he is stupid guy"
+      "remendra ranjan":"he is stupid guy",
+      "Manglesh yadav CV link":`https://drive.google.com/file/d/1RevVPmXZvLaU287RmRupW9Nkclcm2jGc/view?usp=drive_link`
     };
 
      let prompt = `Use the following context to answer the user's question. If the answer is not in the context, respond using your own knowledge.
     Context:
     - About your creator: ${context["about your creater"]}
-    - About you: ${context["about you"]}
+    - About you: ${context["about you"]},
+    -manglesh yadav'c CV link: ${context["Manglesh yadav CV link"]}
     User Question: ${userquestion}`;
-    
+
     const result = await model.generateContent(prompt);
 
     const response = await result.response;
